a993c4f7bb87f4254477e2efb1fd29b445e2d156
Mahesh <158261935+shelakeemahesh@users.noreply.github.com>
2026-06-17 17:45:38 +0530

    Fix duplicate context path prefix /api/v1.0 in FriendExpenseController

--- src/main/java/in/maheshshelakee/moneymanager/controller/FriendExpenseController.java
+++ src/main/java/in/maheshshelakee/moneymanager/controller/FriendExpenseController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1.0/friend-expenses")
+@RequestMapping("/friend-expenses")
 @RequiredArgsConstructor
 public class FriendExpenseController {
 
